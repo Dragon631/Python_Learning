66fdf33f83bfd23272602d520b415193aab957f7
Dragon631 <zyl631@126.com>
2019-04-02 16:16:26 +0800

    修改“内置模块” 为"Built-in module" 编写shelve方法实现数据存储

---
+++ Built-in module/shelve_module.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   File Name:     shelve_module
+   Description :
+   Author :       a
+   date:          2019/4/2
+-------------------------------------------------
+"""
+__author__ = 'a'
+
+"""
+Shelve是对象持久化保存方法，将对象保存到文件里面，缺省（即默认）的数据存储文件是二进制的
+使用时，只需要使用open函数获取一个shelf对象，然后对数据进行增删改查操作，
+在完成工作、并且将内存存储到磁盘中，最后调用close函数变回将数据写入文件
+"""
+
+import shelve
+
+"""
+user_info = [
+    ['Apple', '123456'],
+    ['Banana', '123456'],
+    ['cat', '123456']
+]
+
+infoFile = shelve.open('infoDb.txt')
+infoData = user_info
+infoFile.close()
+
+infoFile = shelve.open('infoDb.txt')
+print(infoData)
+
+for i, v in enumerate(infoData):
+    print(infoData[i][0])
+"""
+
+# 自定义类
+class Student(object):
+    def __init__(self, name, age, sno):
+        self.name = name
+        self.age = age
+        self.sno = sno
+
+    def __repr__(self):
+        return 'Student [name: %s, age: %d, sno: %d]' % (self.name, self.age, self.sno)
+
+Tom = Student('Tom', 25, 201901)
+Adm = Student('Adm', 21, 201902)
+# print(Tom,Adm)
+
+# with shelve.open("info.db") as db:
+db = shelve.open("info.db")
+db['Tom'] = Tom
+db['Adm'] = Adm
+db['Adm'] = Adm
+db.close()
+
+# with shelve.open("info.db") as db:
+db = shelve.open("info.db")
+print(db['Tom'])
+print(db['Adm'])
+print(db.keys())
+# db.pop('Adm')
+db.close()
+
+db = shelve.open("info.db")
+print(db['Adm'])
+db.close()
